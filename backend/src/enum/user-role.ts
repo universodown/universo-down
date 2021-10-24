f8b16d489d916f45c10b72d59b36f0af6fd70c8b
José Carlos dos Anjos <79722929+jcanjosjr@users.noreply.github.com>
2021-10-24 15:47:15 -0300

    Delete backend/src/enum directory

--- backend/src/enum/user-role.ts
+++
@@ -1,5 +0,0 @@
-export enum UserRole {
-    Admin = 'admin',
-    Member = 'member',
-    Owner = 'owner'
-}
--- backend/src/enum/weekday.ts
+++
@@ -1,9 +0,0 @@
-export enum Weekday {
-    Monday = 'monday',
-    Tuesday = 'tuesday',
-    Wednesday = 'wednesday',
-    Thursday = 'thursday',
-    Friday = 'friday',
-    Saturday = 'saturday',
-    Sunday = 'sunday'
-}
