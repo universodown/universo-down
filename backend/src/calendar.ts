f340cce504e9c728d31820c566c90211265ae733
José Carlos dos Anjos <79722929+jcanjosjr@users.noreply.github.com>
2021-10-24 15:46:23 -0300

    change enum, update imports

---
+++ backend/src/calendar.ts
@@ -0,0 +1,33 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    JoinColumn,
+    OneToMany
+} from 'typeorm'
+
+import { User } from './user'
+import { Weekday } from './enum/weekday'
+
+@Entity({ name: 'calendar' })
+export class Calendar {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column('enum', { enum: Weekday, default: 'member' })
+    dayOfWeek: string
+
+    @Column('text', { name: 'start_hour' })
+    startHour: string
+
+    @Column('text', { name: 'end_hour' })
+    endHour: string
+
+    @Column('int', { name: 'user_id', nullable: false })
+    userId: number
+
+    @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+    @OneToMany(_ => User, u => u.calendars, { onDelete: 'CASCADE' })
+    user: User
+}
---
+++ backend/src/user.ts
@@ -0,0 +1,45 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    JoinColumn,
+    OneToMany
+} from 'typeorm'
+
+import { UserRole } from './enum/user-role'
+import { Organization } from './organization'
+import { Calendar } from './calendar'
+
+@Entity({ name: 'users' })
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column('text', { name: 'first_name' })
+    firstName: string
+
+    @Column('text', { name: 'last_name' })
+    lastName: string
+
+    @Column('text')
+    email: string
+
+    @Column('text')
+    password: string
+
+    @Column('enum', { enum: UserRole, default: 'member' })
+    role: UserRole
+
+    @Column('int', { name: 'organization_id', nullable: false })
+    organizationId: number
+
+    @JoinColumn({ name: 'organization_id', referencedColumnName: 'id' })
+    @ManyToOne(_ => Organization, o => o.users, { onDelete: 'CASCADE' })
+    organization: Organization
+
+    @ManyToOne(_ => Calendar, c => c.user, { cascade: true })
+    calendars: Calendar[]
+
+}
