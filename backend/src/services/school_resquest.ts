f6dc57df681d93df393819b13d39ccfc82d15699
Jefferson <88409973+BugaroxD@users.noreply.github.com>
2021-11-05 21:58:27 -0300

    Atualizando Service

--- backend/src/services/school_resquest.ts
+++ backend/src/services/school_resquest.ts
@@ -1,18 +1,23 @@
 import { Service } from 'typedi'
-import { getManager } from 'typeorm'
+import { getManager,  EntityManager } from 'typeorm'
 import { InjectRepository } from 'typeorm-typedi-extensions'
 
 import SchoolRequestRepository from '../repositories/school_request'
 import { Context } from '../api/dto/context'
 import { SchoolRequest } from '../model/school-request'
-import { SchoolRequestCreate, SchoolRequestUpdate } from '../api/dto/school_request'
+import { SchoolRequestCreate, SchoolRequestUpdate } from '../api/dto/school-request'
 
 @Service()
 export default class SchoolRequestService {
 
     @InjectRepository(SchoolRequestRepository)
     private readonly repository: SchoolRequestRepository
-
+    
+    validateDates(date: Date, responseDate?: Date): void {
+        if (responseDate && date > responseDate) {
+            throw new Error('A data de solicitação não pode ser maior que a data de resposta')
+        }
+    }
 
     async create(
         context: Context, 
@@ -22,47 +27,53 @@ export default class SchoolRequestService {
         return getManager().transaction(async db => {
             const repository = db.getRepository(SchoolRequestRepository)
 
-            return repository.save({
-                ...schoolrequestInfo,
+             return repository.save({
+                ...transportRequestInfo,
+                date: new Date(),
                 organizationId: context.organization.id
             })
         })
     }
 
     async update(
-        context: Context, 
+        _: Context, 
         id: number,
         schoolrequestInfo: SchoolRequestUpdate
-        ) {
+    ){
+        const transportRequest = await this.findById(id)
+        this.validateDates(transportRequest.date, transportRequestInfo.responseDate)    
+            
         return getManager().transaction(async db => {
             const repository = db.getRepository(SchoolRequestRepository)
         
                 return repository.save({
                 ...schoolrequestInfo,
                 id,
-                organizationId: context.organization.id
             })
         })
     }
 
-    async delete(id: number): Promise<SchoolRequest> {
-        return getManager().transaction(async db => {
-            const repository = db.getCustomRepository(SchoolRequestRepository)
-            const user = await repository.findById(id, db)
-
-            return repository.remove(user)
+    async delete(
+        id: number
+    ){
+        return getManager().transaction(async db =>{
+            const repository = db.getCustomRepository(TransportRequestRepository)
+            const transportRequest = await repository.findById(id, db)
+            return repository.remove(transportRequest)
         })
     }
 
     async findById(
-        id: number
-        ) {
-        return this.repository.findById(id)
+        id: number,
+        db?: EntityManager
+    ){
+        return this.repository.findById(id, db)
     }
 
     async findAll(
-        context: Context
-        ) {
-            return this.repository.findAll(context)
+        context: Context,
+        assistedId: number
+    ){
+        return this.repository.findAll(context, assistedId)
     }
 }
